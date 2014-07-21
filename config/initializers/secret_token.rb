e779c89839c8433552ba17391d663b6a82a78f8b
Han Kim <hankim813@gmail.com>
2014-07-21 01:20:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whoshoopin::Application.config.secret_key_base = 'a279f59b20cced0446d53ffd6b31a9d13e096999d3c9f94c2a5fe5f3af1db3cd87a33eabe4c73d509fc89d97cd341b1643a11527fcbb20c81e53ec2386f8df54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Whoshoopin::Application.config.session_store :cookie_store, key: '_whoshoopin_session'
